8d109e96a456649681f01c2acb78b366553b140a
Derek Mercedes <mercedes.derek@gmail.com>
2024-07-26 08:26:51 -0600

    image added, home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
-    <h1>I'm Derek Mercedes. I make things for computers</h1>
+    <div class = "wrapper">
+    <div class = "text">
+    <h1>I'm Derek Mercedes. </h1>
+    <h1>I make things for computers</h1>
+    </div>
+    <div class = "image">
+    <img src="../assets/derek.png" alt="derek" width="250">
+    </div>
+    </div>
   </div>
 </template>
 
 
 <style>
+
+.wrapper {
+  display: flex;
+  justify-content:  space-between;
+  margin: 5rem;
+}
 
 @media (min-width: 1024px) {
   .home {
