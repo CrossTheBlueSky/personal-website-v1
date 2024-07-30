2a04254adc6cd694701bf364e4bb7961174be99b
Derek Mercedes <mercedes.derek@gmail.com>
2024-07-29 18:10:59 -0600

    retro site fully scaffolded, nginx implemented for future use in modern site and SPAs

--- Retro/src/views/AboutView.vue
+++ Retro/src/views/AboutView.vue
@@ -1,14 +1,23 @@
 <template>
+  <RouterLink to="/">Back to Home</RouterLink>
   <div class="about">
-    <h1>This is an about page. It might break when I make the text longer</h1>
+    <h1>About Me</h1>
+    <p>
+    Hey there! I'm Derek Mercedes, a Full Stack Developer based in Denver, Colorado. 
+    My journey into tech has been a bit of an adventure - from crafting AI for video games at Full Sail University to managing teams in the hospitality industry, and finally finding my true calling in web development. 
+    When I'm not coding, you'll find me doing circus acrobatics, playing video and tabletop games, and spending time with my dog.
+    I believe in the power of technology to solve real-world problems and make life a little more fun along the way. If it can make a difference, I'm all in! 
+    So if you're looking for someone who combines technical know-how with creativity and a love for other humans, you're in the right place. 
+    Let's create something awesome together!
+    </p>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
+    margin: 25px;
     min-height: 100vh;
-    display: flex;
     align-items: center;
   }
 }
--- Retro/src/views/ContactView.vue
+++ Retro/src/views/ContactView.vue
@@ -1,16 +1,26 @@
 <template>
+  <RouterLink to="/">Back to Home</RouterLink>
     <div class="contact">
-      <h1>This is a contact page</h1>
+      <h1 style="margin-bottom: 10px;"> Find me on:</h1>
+      <a href="mailto:mercedes.derek@gmail.com">Email</a>
+      <br>
+      <a href="https://www.linkedin.com/in/derek-mercedes">Linkedin</a>
+      <br>
+      <a href="https://github.com/crossthebluesky">Github</a>
+      <br>
+      <a href="https://dev.to/crossthebluesky">Dev.to</a>
     </div>
   </template>
-  
   
   <style>
   
   @media (min-width: 1024px) {
     .contact {
-      min-height: 100vh;
       display: flex;
+      flex-direction:column;
+      font-size: 20px;
+      margin: 50px;
+      min-height: 100vh;
       align-items: center;
     }
   }
