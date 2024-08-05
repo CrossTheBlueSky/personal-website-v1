08be1b02b5a14f554e0a6652d45b14628d0b87e6
Derek Mercedes <mercedes.derek@gmail.com>
2024-08-05 15:50:34 -0600

    more route changes

--- retro/vite.config.ts
+++ retro/vite.config.ts
@@ -10,10 +10,5 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
-  base: '/retro/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  base: '/retro/'
 })
