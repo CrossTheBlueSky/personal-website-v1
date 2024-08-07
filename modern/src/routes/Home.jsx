7b032e2c3f7346f7dbff775e2c77d0ab91be20cb
Derek Mercedes <mercedes.derek@gmail.com>
2024-08-06 18:51:02 -0600

    weird black triangle

--- modern/src/routes/Home.jsx
+++ modern/src/routes/Home.jsx
@@ -5,13 +5,13 @@ function Home(){
 
     return (
         <>
-        <SimpleGrid mx='50px' cols={2}>
+        <SimpleGrid mx='auto' cols={2}>
             <div>
                 <h1>I&apos;m Derek Mercedes</h1>
                 <p>I make things for computers</p>
             </div>
             <div>
-            <Image  h='400px'src='modern/public/derek-no-scanlines.jpg' alt="derek-mercedes" />
+            <Image  h='25rem'src='modern/public/derek-no-scanlines.jpg' alt="derek-mercedes" />
             </div>
         </SimpleGrid>
         </>
