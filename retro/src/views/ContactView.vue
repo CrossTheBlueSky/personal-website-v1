9dafe3fb8bbe47b763195d7a8be014a60d53de3e
Derek Mercedes <mercedes.derek@gmail.com>
2024-08-07 16:21:40 -0600

    finished with routes, removed power button

--- retro/src/views/ContactView.vue
+++ retro/src/views/ContactView.vue
@@ -1,29 +1,66 @@
 <template>
-  <RouterLink to="/">Back to Home</RouterLink>
+  <div class ="contact-container-wrapper">
+  <div class="contact-container">
+  <RouterLink class="return" to="/">Back to Home</RouterLink>
+  <h1 style="margin-bottom: 2rem;"> Find me on:</h1>
     <div class="contact">
-      <h1 style="margin-bottom: 10px;"> Find me on:</h1>
+      <div class="contact-row">
+        <img src="/src/assets/gmail.svg" alt="gmail" width="50px" height="50px">
       <a href="mailto:mercedes.derek@gmail.com">Email</a>
-      <br>
-      <a href="https://www.linkedin.com/in/derek-mercedes">Linkedin</a>
-      <br>
-      <a href="https://github.com/crossthebluesky">Github</a>
-      <br>
-      <a href="https://dev.to/crossthebluesky">Dev.to</a>
+      </div>
+      <div class="contact-row">
+        <img src="/src/assets/linkedin.svg" alt="linkedin" width="50px" height="50px">
+      <a icon="" href="https://www.linkedin.com/in/derek-mercedes">Linkedin</a>
     </div>
+    <div class="contact-row">
+      <img src="/src/assets/github.svg" alt="github" width="50px" height="50px">
+      <a icon="github" href="https://github.com/crossthebluesky">Github</a>
+  </div>
+    <div class="contact-row">
+      <img src="/src/assets/dev.to.svg" alt="dev.to" width="50px" height="50px">
+      <a icon="dev" href="https://dev.to/crossthebluesky">Dev.to</a>
+    </div>
+    </div>
+  </div>
+  </div>
   </template>
   
   <style>
+
+    .return{
+      margin-left: 8rem;
+      width: 100%;
+    }
+
+    .contact-container-wrapper {
+      align-items: start;
+      justify-content: center;
+      flex-direction: column;
+      width: 53vw;
+      height: 58vh;
+    }
+    .contact-container {
+      display: flex;
+  flex-direction: column;
+  height:100%;
+  align-items: center;
+  font-size: 1.2rem
+    }
   
-  @media (min-width: 1024px) {
     .contact {
       display: flex;
-      flex-direction:column;
-      font-size: 1.2rem;
-        margin: 3rem;
-        min-height: 100vh;
-        
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+      justify-content: space-evenly;
       align-items: center;
     }
-  }
-  
+    .contact-row {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 1rem;
+
+    }
+
   </style>
